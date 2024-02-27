b21fd652808058fcef5d8fab760b06b28510ce19
Franco <francodeanquin@gmail.com>
2024-02-27 16:53:17 -0300

    auth

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import userRouter from "./routes/user.route.js"
 dotenv.config()
+import authRouter from "./routes/auth.route.js"
+const app = express();
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO)
 .then(() => {
@@ -11,7 +15,9 @@ mongoose.connect(process.env.MONGO)
     console.error("Error connecting to MongoDB:", err.message);
 });
 
-const app = express();
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+
 
 app.listen(4000, () => {
   console.log("server is running on port 4000!!!");
