d515fbde7cd31501228a23d7044368302c654fc4
Franco <francodeanquin@gmail.com>
2024-02-27 19:15:31 -0300

    added error middleware

--- api/index.js
+++ api/index.js
@@ -1,23 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-import userRouter from "./routes/user.route.js"
-dotenv.config()
-import authRouter from "./routes/auth.route.js"
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+dotenv.config();
+import authRouter from "./routes/auth.route.js";
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDB");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.error("Error connecting to MongoDB:", err.message);
+  });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false.statusCode,
+    message,
+  });
 });
-
-app.use("/api/user", userRouter)
-app.use("/api/auth", authRouter)
-
 
 app.listen(4000, () => {
   console.log("server is running on port 4000!!!");
