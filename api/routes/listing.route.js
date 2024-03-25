8739bae471e97285b915f3df688c8b92ac57076f
Franco <francodeanquin@gmail.com>
2024-03-25 16:57:26 -0300

    complete update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,11 +1,18 @@
-import {Router} from "express"
-import { createListing, deleteListing, updateListing } from "../controllers/listing.controller.js"
+import { Router } from "express";
+import {
+  createListing,
+  deleteListing,
+  updateListing,
+  getListing,
+  getListings,
+} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
-const router = Router()
+const router = Router();
 
-router.post("/create",verifyToken, createListing)
-router.delete("/delete/:id", verifyToken, deleteListing)
-router.post("/update/:id", verifyToken, updateListing)
-
+router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
